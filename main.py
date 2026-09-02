8276112769aee53ed8500733d816b67d9087c43a
Daniel Adeyemi <danieladeyd77@gmail.com>
2026-09-02 02:46:34 +0100

    fetching products

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI application!"}
+
+
+products = [
+    {"id": 1, "name": "Laptop", "price": 10099},
+    {"id": 2, "name": "mobile", "price": 12999},
+    {"id": 3, "name": "Laptop", "price": 15999},
+]
+
+
+@app.get("/products")
+def get_products():
+    return {"products": products}
